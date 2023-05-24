6c3434f5b7e137e319178b16a2c678dbc93e14b7
Denis Romodanov <darvenommm@gmail.com>
2023-05-24 22:48:27 +0300

    fix: multer setFileName function

--- server/middlewares/library/multer.ts
+++ server/middlewares/library/multer.ts
@@ -1,11 +1,18 @@
 import multer from 'multer';
 import bytes from 'bytes';
-import { resolve } from 'node:path';
+import {
+  resolve,
+  extname as getExtname,
+  basename as getBasename,
+} from 'node:path';
 
 const storage = multer.diskStorage({
   destination: resolve('static', 'photos'),
   filename: (_, file, cb) => {
-    cb(null, `${file.fieldname}-${Date.now()}`);
+    const basename = getBasename(file.originalname);
+    const extname = getExtname(file.originalname);
+
+    cb(null, `${basename}-${Date.now()}${extname}`);
   },
 });
 
